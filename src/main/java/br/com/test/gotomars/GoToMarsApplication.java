7c8209da7c6fdc87a62c44f01db80b201079a5a2
digao1297 <digao1297@gmail.com>
2021-11-24 09:37:37 -0300

    :tada: initial commit

---
+++ src/main/java/br/com/test/gotomars/GoToMarsApplication.java
@@ -0,0 +1,13 @@
+package br.com.test.gotomars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoToMarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoToMarsApplication.class, args);
+	}
+
+}
