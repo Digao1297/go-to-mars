d1007a39c9a41fbd00323e03246d363d9582d30d
digao1297 <digao1297@gmail.com>
2021-11-25 09:59:36 -0300

    🚚 Renomeando os usecases

--- src/main/java/br/com/test/gotomars/domain/usecases/LandProbeUsecase.java
+++ src/main/java/br/com/test/gotomars/domain/usecases/LandProbeUsecase.java
@@ -1,8 +1,8 @@
-package br.com.test.gotomars.domain.usecase;
+package br.com.test.gotomars.domain.usecases;
 
 import br.com.test.gotomars.domain.entities.ProbeEntity;
 import br.com.test.gotomars.domain.entities.QuadrantEntity;
 
-public interface LandProbe {
+public interface LandProbeUsecase {
     QuadrantEntity execute(ProbeEntity entity);
 }
