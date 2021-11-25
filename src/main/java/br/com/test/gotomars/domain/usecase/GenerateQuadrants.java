cabe58e36c860623a208da0e60d623b5907c232a
digao1297 <digao1297@gmail.com>
2021-11-25 09:32:09 -0300

    🚀 implementado a interface GenerateQuadrants que impõe a implementação do metodo responsável por gerar o plano

---
+++ src/main/java/br/com/test/gotomars/domain/usecase/GenerateQuadrants.java
@@ -0,0 +1,7 @@
+package br.com.test.gotomars.domain.usecase;
+
+import br.com.test.gotomars.domain.entities.QuadrantEntity;
+
+public interface GenerateQuadrants {
+    void execute(QuadrantEntity entity);
+}
