af27534c335501b85190c552ecc7d019dfbb73a2
digao1297 <digao1297@gmail.com>
2021-12-06 09:27:43 -0300

    ✅ criado teste para verificar a busca de todos os quadrantes

---
+++ src/test/java/br/com/test/gotomars/data/usecases/FindAllQuadrantsUsecaseImplTest.java
@@ -0,0 +1,37 @@
+package br.com.test.gotomars.data.usecases;
+
+import br.com.test.gotomars.domain.entities.QuadrantEntity;
+import br.com.test.gotomars.domain.repositories.QuadrantRepository;
+import br.com.test.gotomars.domain.usecases.FindAllQuadrantsUsecase;
+import br.com.test.gotomars.domain.usecases.GenerateQuadrantsUsecase;
+import br.com.test.gotomars.infra.repositories.LocalQuadrantRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = FindAllQuadrantsUsecaseImplTest.class)
+public class FindAllQuadrantsUsecaseImplTest {
+
+    private GenerateQuadrantsUsecase generateQuadrantsUsecase;
+    private FindAllQuadrantsUsecase findAllQuadrantsUsecase;
+
+    @BeforeEach
+    void setup() {
+
+        QuadrantRepository quadrantRepository = new LocalQuadrantRepository();
+
+        findAllQuadrantsUsecase = new FindAllQuadrantsUsecaseImpl(quadrantRepository);
+        generateQuadrantsUsecase = new GenerateQuadrantsUsecaseImpl(quadrantRepository);
+
+        generateQuadrantsUsecase.execute();
+    }
+
+    @Test()
+    void findAllQuadrants() {
+        QuadrantEntity[][] entity = findAllQuadrantsUsecase.execute();
+
+        Assertions.assertNotNull(entity);
+    }
+
+}
